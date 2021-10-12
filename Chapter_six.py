7212f24642c6836827b0d24b9e1787e70355555f
XCanL996 <86374458+XCanL996@users.noreply.github.com>
2021-10-12 08:50:40 -0400

    New Chapter
    Chapter 6 completed, Chapter 7 Started

--- Chapter_six.py
+++ Chapter_six.py
@@ -4,20 +4,84 @@
 # Set in Python
 
 # Creating a Set
-set1 = set()
-print("Initial blank Set: ")
-print(set1)
+# set1 = set()
+# print("Initial blank Set: ")
+# print(set1)
+#
+# # Creating a Set with
+# # the use of Constructor
+# # (Using object to Store String)
+# String = 'GeeksForGeeks'
+# set1 = set(String)
+# print("\nSet with the use of an Object: ")
+# print(set1)
+#
+# # Creating a Set with
+# # the use of a List
+# set1 = set(["Geeks", "For", "Geeks"])
+# print("\nSet with the use of List: ")
+# print(set1)
+#
+#
+# #6-1. Person:
+#
+# Person = {'first_name':'Bin', 'last_name' : 'Zhang', 'age' : 30 , 'city' : 'Toronto'}
+#
+# print(Person)
+#
+# #6-2. Favorite Numbers:
+#
+# favorite_numbers = {'Zhang' : 5, 'Yu' : 1, 'Zheng' : 2, 'Mike' : 3, 'Tom' : 3}
+#
+# for key in favorite_numbers:
+#     print(key + "'s favorite number is " + str(favorite_numbers[key]))
 
-# Creating a Set with
-# the use of Constructor
-# (Using object to Store String)
-String = 'GeeksForGeeks'
-set1 = set(String)
-print("\nSet with the use of an Object: ")
-print(set1)
+#6-4. Glossary 2
 
-# Creating a Set with
-# the use of a List
-set1 = set(["Geeks", "For", "Geeks"])
-print("\nSet with the use of List: ")
-print(set1)
+# #6-5. Rivers:
+#
+# Rivers = {"River A":"City A", "River B":"City B", "River C":"City C", "River D":"City D"}
+#
+# for key, value in Rivers.items():
+#     print(key + " runs through " + value + ".\n")
+
+#6-7. People:
+#
+# people_1 = {"First name" : "Mike", "age" : 30}
+# people_2 = {"First name" : "Natt", "age" : 31}
+# people_3 = {"First name" : "Joe", "age" : 32}
+#
+# people = [people_1, people_2, people_3]
+#
+# for person in people:
+#     print(person["First name"] + " is " + str(person["age"]) + " years old.\n")
+
+# pets
+
+# pets = []
+#
+# pet_1 = {"name" : "Pet one" , "age" : 1}
+# pet_2 = {"name" : "Pet two" , "age" : 2}
+# pet_3 = {"name" : "Pet three" , "age" : 3}
+# pet_4 = {"name" : "Pet four" , "age" : 4}
+#
+# for pet in pets:
+#     print(pet.["name"] + " is " + pet.["age"] +)
+#
+#
+
+#6-11. Cities:
+
+# city_1 = {"country":"China","population":"543534","fact":" fact one"}
+# city_2 = {"country":"India","population":"2342342","fact":" fact two"}
+# city_3 = {"country":"North Korea","population":"325345","fact":" fact three"}
+# city_4 = {"country":"South Korea","population":"124235","fact":" fact four"}
+#
+# cities = {"Beijing" : city_1, "city two": city_2, "city three": city_3, "city_four": city_4}
+#
+# for city, information in cities.items():
+#     print(city + " is located in " + information["country"] + ", its populations is " + information["population"]
+#           + ", and i know some facts about this city, which is " + information["fact"] + ".")
+
+#6-12. Extensions
+
